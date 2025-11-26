2df08e99bd7d8ec5783234f95f7fa0f734a61ce2
Endurance <endurancechia@gmail.com>
2025-11-26 12:39:56 +0100

    implemented the system info dashboard

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # DASHBOARD
+the system info dashboard is an application built with bash that helps provide system info
+
+# 1 system info
+provides info on 
+# current user
+# host
+# date and time
+
+# 2 up time
+tells the user how long the user how long the computer has been on 
+
+# 3 memory usage 
+provides information on memory usage 
+
+# 4 disk usage 
+provides information about the disk like;
+total disks space,
+total used 
+and free storage on disk
+
+# 5 running processes 
+this section provides the total number of running processes and the top 5 memory consuming processes 
--- sysinfo.sh
+++ sysinfo.sh
@@ -1,1 +1,63 @@
 #!/bin/bash
+
+#system Identity
+echo
+echo "=========SYSTEM IDENTITY========="
+echo
+
+echo "current user:   $(whoami)"                               # $whoami prints current user
+
+echo "Hostname:       $(hostname)"                             # $hostname prints the host/computer name
+
+echo "date/time:      $(date)"                                 # $date prints the date and time 
+
+
+echo
+# uptime
+
+echo "=========UPTIME=================="
+echo
+
+echo "Up_time:        $(uptime -p)"                             # $uptime prints the time the pc has been on
+
+
+
+echo 
+#Memory Usage 
+
+echo "=========MEMORY USAGE============="
+echo
+
+                                                          
+MEM_TOTAL=$(free -m | awk '/Mem:/ {print $2}')           # $2 prints the total memory
+MEM_USED=$(free -m | awk '/Mem:/ {print $3}')            # awk mem extract the needed memory information
+MEM_FREE=$(free -m | awk '/Mem:/ {print $4}')            # free -m allows user to check RAM info
+
+echo "Total: $MEM_TOTAL "
+echo "Used:  $MEM_USED "
+echo "Free:  $MEM_FREE"
+
+echo
+# Disk Usage
+echo "==========DISK USAGE================"
+echo
+
+DISK_TOTAL=$(df -h / | awk 'NR==2 {print $2}')          #df disk file 
+DISK_USED=$(df -h / | awk 'NR==2 {print $3}')           # h readable by human
+DISK_FREE=$(df -h / | awk 'NR==2 {print $4}')        #print $1 not used because it identifies the system
+
+
+echo "Total: $DISK_TOTAL"
+echo  "Used: $DISK_USED "
+echo "Free: $DISK_FREE"
+
+#Running processes 
+echo
+echo "------total running processes------"
+echo
+TOTAL_PROC=$(ps aux | wc -l)                          #ps aux show all running processes
+echo "Running processes: $TOTAL_PROC"                  #wc -l  count the lines and number of processes
+echo
+
+TOP_5_MEM_CON_PROC=$(ps aux | sort -nrk 4 | head -6)      # head -6 displays only the top 5 
+echo "top 5 memory consuming processes: $TOP_5_MEM_CON_PROC"
